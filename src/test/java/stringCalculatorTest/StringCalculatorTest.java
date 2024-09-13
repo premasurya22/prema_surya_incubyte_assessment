0dd15319c9578ca5fe873b445bf61abf3164edcb
premasshetty <premasshetty2206@gmail.com>
2024-09-13 14:02:09 +0530

    Code refactoring in Test class to have single object of StringCalculator class

--- src/test/java/stringCalculatorTest/StringCalculatorTest.java
+++ src/test/java/stringCalculatorTest/StringCalculatorTest.java
@@ -6,45 +6,39 @@ import stringCalculator.NegativeNumberException;
 
 public class StringCalculatorTest {
 
+	StringCalculator calculator = new StringCalculator();
 	@Test
-	public void testEmptyString() throws Exception{
-		StringCalculator calculator = new StringCalculator();
+	public void testEmptyString() throws Exception{	
 		assertEquals(0, calculator.add(""));
 	}
 	
 	 @Test
 	    public void testSingleNumber() throws Exception {
-	        StringCalculator calculator = new StringCalculator();
 	        assertEquals(1, calculator.add("1"));
 	    }
 	 
 	 @Test
 	 public void testTwoNumbers() throws Exception{
-		 StringCalculator calculator = new StringCalculator();
 		 assertEquals(6, calculator.add("2,4"));
 	 }
 	 
 	 @Test
 	 public void testMultipleNumbers() throws Exception{
-		 StringCalculator calculator = new StringCalculator();
 		 assertEquals(17, calculator.add("2,4,3,8"));
 	 }
 	 
 	 @Test
 	    public void testNewLineDelimiter() throws Exception {
-	        StringCalculator calculator = new StringCalculator();
 	        assertEquals(9, calculator.add("1\n2,3,3"));
 	    }
 	 
 	 @Test
 	    public void testCustomDelimiter() throws Exception {
-	        StringCalculator calculator = new StringCalculator();
 	        assertEquals(3, calculator.add("//;\n1;2"));
 	    }
 	 
 	 @Test
 	    public void testNegativeNumbers() {
-	        StringCalculator calculator = new StringCalculator();
 	        Exception exception = assertThrows(NegativeNumberException.class, () -> {
 	            calculator.add("1,-2,3,-4");
 	        });
